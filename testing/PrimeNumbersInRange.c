043447664815a7ff16490ce24be0f65eeec68bcf
arittASinha2003 <93367827+arittASinha2003@users.noreply.github.com>
2022-03-13 14:18:19 +0530

    Add files via upload

---
+++ testing/PrimeNumbersInRange.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+int isPrime(int n)
+{
+    int i;
+    
+    for (i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main()
+{
+    int n1, n2, i;
+    
+    printf("Enter lower range: ");
+    scanf("%d", &n1);
+    
+    printf("Enter upper range: ");
+    scanf("%d", &n2);
+    
+    printf("Prime numbers between %d and %d are: ", n1, n2);
+    
+    for (i = n1; i <= n2; i++)
+    {
+        if (isPrime(i))
+        {
+            printf("%d ", i);
+        }
+    }
+    return 0;
+}
